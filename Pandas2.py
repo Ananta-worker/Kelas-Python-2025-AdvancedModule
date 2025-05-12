d023b8368bf0c5e4cb4b6b2f0e08aea773508fe1
Purhita <purhito@gmail.com>
2025-05-13 06:57:54 +0900

    pandas2 lanjutan

--- Pandas2.py
+++ Pandas2.py
@@ -3,6 +3,7 @@ Pandas 2
 """
 import pandas as pd
 import random
+import matplotlib.pyplot as plt
 
 data_nilai = {
     "Nama": ["Ansar", "Bombom", "Caca", "Didu", "Eeb", "Fitri"],
@@ -44,6 +45,27 @@ print(pd.DataFrame(murid_baru, index=[6]))
 df_nilai = pd.concat([df_nilai, pd.DataFrame(murid_baru, index=[6])])
 print(df_nilai)     # menggabungkan tabel
 
-df_nilai.drop("Keterampilan", axis=1, inplace=True)
+df_nilai.drop("Keterampilan", axis=1, inplace=True)     # menghapus kolom
 print(df_nilai)
 
+df_nilai.drop(5, axis=0, inplace=True)      # menghapus baris
+print(df_nilai)
+# mengambil data dari table excel, install dulu openpyxl, pip install openpyxl / lewat seting
+df_murid = pd.read_excel("daftar nilai.xlsx", index_col="No")
+print(df_murid)
+
+# Membuat grafik/plot dari tabel
+# sebelumnya install dulu lewat terminal - pip install pandas matplotlib
+# import matplotlib.pyplot as plt
+
+df_murid.plot(x="Nama", y="Matematika", kind="barh")    # barh (bar arah horisontal) bar (bar arah vertikal)
+plt.title("Nilai Matematika Siswa")
+plt.xlabel("Nilai Matematika")
+plt.ylabel("Nama siswa")
+plt.show()
+
+df_murid.plot(x="Nama", y=["Matematika", "Bahasa"], kind="barh")    # barh (bar arah horisontal) bar (bar arah vertikal)
+plt.title("Nilai Matematika Siswa")
+plt.xlabel("Nilai Matematika")
+plt.ylabel("Nama siswa")
+plt.show()
